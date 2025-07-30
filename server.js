f40d02b6110c252e64f550d2a82b8f2227af0f6c
Serhii Yemets <115184910+Sergey432654@users.noreply.github.com>
2025-07-30 23:39:36 +0200

    fixxxxx

--- server.js
+++ server.js
@@ -8,20 +8,25 @@ app.get('/api/elements',(req,res) => {
     res.json([
         {
             id:1,
-            title:'About us',
+            title:'Your teacher is',
+            name:"Ozzy",
+            predmet:"Eanglish",
             content:"We are a group of developers...",
-            penis: 'gsgsfgsgsdfgsdfg',
             picture:"https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/rockcms/2025-07/250706-black-sabbath-4-rs-b6e801.jpg"
         },
         {
             id: 2,
             title: 'Quick Links',
+            name:"serhii",
+            predmet:"Eanglish",
             content: 'пеніа, іпіапі, івапів',
             picture:"https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/rockcms/2025-07/250706-black-sabbath-4-rs-b6e801.jpg",
         },
         {
             id: 3,
             title: 'Contact',
+            name:"",
+            predmet:"Eanglish",
             content: 'sergeyzt50@gmail.com',
             picture:"https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/rockcms/2025-07/250706-black-sabbath-4-rs-b6e801.jpg"
         }
@@ -31,7 +36,6 @@ app.get('/api/elements',(req,res) => {
 app.get('/', (req, res) => {
     res.send('rgrgwrgwgwrgwrg');
 })
-
 app.listen(8000, () => {
     console.log('Server is running on http://localhost:8000');
 });
